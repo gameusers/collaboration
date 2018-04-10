c4afe988f031f7842a08c580e136db7d4c549e38
cloud9 <mail@gameusers.org>
2018-04-10 08:31:57 +0000

    React導入テスト

--- app.js
+++ app.js
@@ -1,85 +1,47 @@
 // --------------------------------------------------
-//   Import
+//   Require
 // --------------------------------------------------
 
-require('dotenv').config();
+// var createError = require('http-errors');
 
-const http = require('http');
 const express = require('express');
 const path = require('path');
-const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const fileUpload = require('express-fileupload');
+// const fileUpload = require('express-fileupload');
 
-const passport = require('passport');
-const TwitterStrategy = require('passport-twitter').Strategy;
-const LocalStrategy = require('passport-local').Strategy;
-const session = require('express-session');
-const MongoStore = require('connect-mongo')(session);
-const flash = require('connect-flash');
+// const passport = require('passport');
+// const TwitterStrategy = require('passport-twitter').Strategy;
+// const LocalStrategy = require('passport-local').Strategy;
+// const session = require('express-session');
+// const MongoStore = require('connect-mongo')(session);
+// const flash = require('connect-flash');
 
 // const logger = require('./lib/logger');
-const log = require('./lib/error_logger');
+// const log = require('./lib/error_logger');
 
-const helmet = require('helmet');
-const csrf = require('csurf');
-// const cors = require('cors');
+// const helmet = require('helmet');
+// const csrf = require('csurf');
+
+// var cookieParser = require('cookie-parser');
+// var logger = require('morgan');
 
 
-// --------------------------------------------------
-//   定数
-// --------------------------------------------------
 
-const twitterConfigObj = {
-  consumerKey: process.env.TWITTER_CONSUMER_KEY,
-  consumerSecret: process.env.TWITTER_CONSUMER_SECRET,
-  callbackURL: process.env.TWITTER_CALLBACK_URL
+let normalFunc = function(x) {
+    console.log(x);
 };
 
-
-// --------------------------------------------------
-//   Model
-// --------------------------------------------------
-
-const ModelUsers = require('./schema/users');
-const ModelGames = require('./schema/games');
+var test = 1;
+test;
 
 
-// --------------------------------------------------
-//   Middleware Settings
-// --------------------------------------------------
 
-const app = express();
+// ---------------------------------------------
+//   Require: Router
+// ---------------------------------------------
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
-
-app.use(session({
-  secret: 'nPv8ip9MYiNcxBJwmgwHW9pqAOksyE87',
-  resave: false,
-  saveUninitialized: false,
-  store: new MongoStore({
-    mongooseConnection: mongoose.connection,
-    db: 'sessions',
-    ttl: 14 * 24 * 60 * 60,
-    // cookie: {
-    //   secure: true
-    // }
-  }),
-}));
-
-app.use(passport.initialize());
-app.use(passport.session());
-app.use(flash());
-
-app.use('/image', express.static(path.join(__dirname, 'image')));
-
-app.use(helmet());
-
-const csrfProtection = csrf();
-
+const indexRouter = require('./routes/index');
+// const usersRouter = require('./routes/users');
 
 
 
@@ -98,314 +60,54 @@ db.once('open', () => {
 });
 
 
+
+
+
+
+
+
 // --------------------------------------------------
-//   View
+//   Set App
 // --------------------------------------------------
+
+const app = express();
+
+
+// ---------------------------------------------
+//   Middleware Settings
+// ---------------------------------------------
+
+// app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+// app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+
+// ---------------------------------------------
+//   View Engine
+// ---------------------------------------------
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 
+// ---------------------------------------------
+//   Router
+// ---------------------------------------------
 
-// --------------------------------------------------
-//   Page: Index
-// --------------------------------------------------
+app.use('*', indexRouter);
+// app.use('/users', usersRouter);
 
-app.get('/', (req, res, next) => {
-  
-  // logger.warn(req.session.user);
-
-  ModelUsers.find({}, (err, dataArr) => {
-    // console.log(dataArr);
-    if (err) throw err;
-    
-    return res.render('index', {
-      userArr: dataArr,
-      user: req.session && req.session.user ? req.session.user : null
-    });
-    
-  });
-
-  // ModelGames.find({}, (err, dataArr) => {
-  //   if (err) throw err;
-  //   return res.render('index', { gameDataArr: dataArr });
-  // });
-
-});
-
-
-// --------------------------------------------------
-//   Page: Login
-//   
-//   Passport 解説サイト
-//   http://knimon-software.github.io/www.passportjs.org/guide/
-//   http://www.passportjs.org/docs/
-// --------------------------------------------------
-
-// --------------------------------------------------
-//   Page: Login / Twitter
-// --------------------------------------------------
-
-// ストラテジーの設定
-passport.use(new TwitterStrategy(twitterConfigObj,
-  (token, tokenSecret, profile, done) => {
-    
-    ModelUsers.findOne({ twitterProfileId: profile.id }, (err, user) => {
-      
-      if (err) {
-        
-        return done(err);
-        
-      } else if (!user) {
-        
-        const _user = {
-          username: profile.displayName,
-          twitterProfileId: profile.id,
-          imagePath: profile.photos[0].value
-        };
-        
-        const instanceModelUsers = new ModelUsers(_user);
-        
-        instanceModelUsers.save((err)=>{
-          if(err) throw err
-          return done(null, instanceModelUsers);
-        });
-        
-      } else {
-        
-        return done(null, user);
-        
-      }
-      
-    });
-  }
-));
-
-app.get('/oauth/twitter', passport.authenticate('twitter'));
-
-app.get('/oauth/twitter/callback', passport.authenticate('twitter'),
-  (req, res, next) => {
-
-    ModelUsers.findOne({ _id: req.session.passport.user }, (err, user) => {
-      
-      // エラーが起こった場合、セッションがない場合は認証ページにリダイレクトする
-      if (err || !req.session) return res.redirect('/oauth/twitter');
-      
-      // セッションに保存
-      req.session.user = {
-        username: user.username,
-        imagePath: user.imagePath
-      };
-      
-      // ログインに成功した場合はトップページにリダイレクト
-      return res.redirect('/');
-      
-    });
-  }
-);
-
-
-// --------------------------------------------------
-//   Page: Login / ID & Password
-// --------------------------------------------------
-
-app.get('/login', (req, res, next) => {
-  return res.render('login');
-});
-
-// ストラテジーの設定
-passport.use(new LocalStrategy(
-  
-  // 検証用コールバックを設定する
-  (username, password, done) => {
-    ModelUsers.findOne({ username: username }, (err, user) => {
-      
-      if (err) {
-        return done(err);
-      }
-      
-      if (!user) {
-        return done(null, false, { message: 'Incorrect username.' });
-      }
-      
-      if (user.password !== password) {
-        return done(null, false, { message: 'Incorrect password.' });
-      }
-      
-      return done(null, user);
-      
-    });
-  }
-));
-
-app.post('/login',
-
-  passport.authenticate('local', {
-    failureRedirect: '/login', // 失敗したときの遷移先
-    failureFlash: true, // 失敗したときにフラッシュメッセージを表示
-    session: true, // セッションを有効にするかどうか
-  }),
-  
-  // 認証に成功すると、この関数が呼び出される。
-  // 認証されたユーザーは `req.user` に含まれている。
-  (req, res) => {
-    ModelUsers.findOne({ _id: req.session.passport.user }, (err, user) => {
-      
-      console.log('ログインに成功しました');
-      console.log('req.user = ' + req.user);
-      console.log('req.session.passport.user = ' + req.session.passport.user);
-      console.log(`err = ${err}`);
-      
-      // エラーが存在する、またはセッションがない場合はログインページにリダイレクト
-      if (err || !req.session) {
-        return res.redirect('/login');
-      }
-
-      // セッションに保存
-      req.session.user = {
-          username: user.username,
-          imagePath: user.imagePath
-      };
-      
-      // トップページにリダイレクト
-      return res.redirect('/');
-      
-    });
-  },
-  
-  
-);
-
-// _idをシリアライズしてセッションに埋め込む
-passport.serializeUser((user, done) => {
-  done(null, user._id);
-});
-
-// シリアライズされた_idをデシリアライズしてユーザーを特定する
-passport.deserializeUser((id, done) => {
-  ModelUsers.findOne({ _id: id }, (err, user) => {
-    done(err, user);
-  });
-});
-
-
-
-// --------------------------------------------------
-//   Page: Signin サムネイル画像をアップロードする
-// --------------------------------------------------
-
-app.get('/signin', (req, res, next) => {
-  return res.render('signin');
-});
-
-app.post('/signin', fileUpload(), (req, res, next) => {
-
-  const image = req.files.image;
-  
-  image.mv(`./image/${image.name}`, (err) => {
-    
-    if (err) throw err;
-    
-    const instanceModelUsers = new ModelUsers({
-      username: req.body.username,
-      password: req.body.password,
-      imagePath: `/image/${image.name}`
-    });
-    
-    instanceModelUsers.save((err2) => {
-      if (err2) throw err2
-      return res.redirect('/');
-    });
-    
-  })
-  
-});
-
-
-// --------------------------------------------------
-//   Page: Update
-// --------------------------------------------------
-
-app.get('/update', csrfProtection, (req, res, next) => {
-  res.render('update', {
-    csrf: req.csrfToken(),
-  });
-});
-
-app.post('/update', fileUpload(), csrfProtection, (req, res, next) => {
-
-  if (req.files && req.files.image) {
-    
-    const image = req.files.image
-
-    image.mv(`./image/${image.name}`, (err) => {
-
-      if (err) throw err;
-
-      const instanceModelGames = new ModelGames({
-        name: req.body.name,
-        score: req.body.score,
-        imagePath: req.session.user.imagePath,
-        twitterImagePath: `/image/${image.name}`,
-      });
-
-      instanceModelGames.save((err2) => {
-        if (err2) throw err2;
-        return res.redirect('/');
-      });
-
-    });
-
-  } else {
-
-    const instanceModelGames = new ModelGames({
-      name: req.body.name,
-      score: req.body.score,
-      imagePath: req.session.user.imagePath,
-    });
-
-    instanceModelGames.save((err) => {
-      if (err) throw err;
-      return res.redirect('/');
-    });
-
-  }
-
-});
-
-
-
-// --------------------------------------------------
-//   Page: テスト
-// --------------------------------------------------
-
-// app.get('/test', (req, res, next) => {
-//   throw new Error('error');
-// });
-
-// app.get('/test', (req, res, next) => {
-//   return next(new Error('error'));
+// catch 404 and forward to error handler
+// app.use(function(req, res, next) {
+//   next(createError(404));
 // });
 
 
 // --------------------------------------------------
-//   Page: CORS
-// --------------------------------------------------
-// origin: 'https://df44294c8853471b8ddd609c09af06f3.vfs.cloud9.us-west-2.amazonaws.com',
-// const corsOptions = {
-//   origin: 'https://gameusers.org',
-//   methods: 'GET, POST'
-// }
-
-// app.get('/products/:id', cors(corsOptions), (req, res, next) => {
-//   res.json({
-//     msg: 'This is CORS-enabled for a whitelited domain.'
-//   })
-// });
-
-
-// --------------------------------------------------
-//   Page: 404
+//   Router: 404
 // --------------------------------------------------
 
 app.use((req, res, next) => {
@@ -421,12 +123,22 @@ app.use((req, res, next) => {
 
 
 // --------------------------------------------------
-//   Error 処理
+//   Error Handler
 // --------------------------------------------------
+
+// app.use(function(err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render('error');
+// });
 
 app.use((err, req, res, next) => {
   
-  log.error(err);
+  // log.error(err);
 
   if (err.code === 'EBADCSRFTOKEN') {
     res.status(403);
@@ -442,10 +154,4 @@ app.use((err, req, res, next) => {
 });
 
 
-// --------------------------------------------------
-//   Server
-// --------------------------------------------------
-
-const server = http.createServer(app);
-
-server.listen('8080');
+module.exports = app;
--- webpack.config.js
+++ webpack.config.js
@@ -1,56 +1,84 @@
+// --------------------------------------------------
+//   Webpack Configuration Ver.4
+//   公式解説: https://webpack.js.org/configuration/
+// --------------------------------------------------
+
 const webpack = require('webpack');
 const path = require('path');
 
-// 環境を記述 development or production
-const env = 'development';
-
-const config = {
-  entry: {
-    root: './public/react/js/entry-root.jsx'
-  },
+module.exports = {
+  
+  // モード（Ver.4から）: 'production' | 'development' | 'none'
+  // package.jsonのscriptsでモードを指定しているので、ここで指定する必要はありません。
+  mode: 'development',
+  
+  // エントリーポイント
+  entry: './views/entry.jsx',
+  
+  // 出力の設定
   output: {
-    path: path.join(__dirname, '/public/react/js'),
-    filename: '[name]-bundle.min.js',
+    
+    // 出力先のパス（Ver.2以降は絶対パスを指定する必要があります）
+    path: path.join(__dirname, 'public/js'),
+    
+    // 出力するファイル名
+    filename: 'bundle.js',
+    
   },
+  
+  // モジュールの設定
   module: {
     rules: [
+      
+      // JSX
       {
         test: /\.jsx$/,
-        exclude: /node_modules/,
+        exclude: [
+          path.resolve(__dirname, '/node_modules/')
+        ],
         loader: 'babel-loader'
       },
+      
+      // CSS
       {
         test: /\.css$/,
         use: [
           'style-loader',
-          { loader: 'css-loader', options: { importLoaders: 1 } },
+          {
+            loader: 'css-loader',
+            options: {
+              
+              // 0 => no loaders (default);
+              // 1 => postcss-loader;
+              // 2 => postcss-loader, sass-loader
+              importLoaders: 1
+            }
+          },
           'postcss-loader'
         ]
       }
+      
     ]
   },
+  
   resolve: {
     extensions: ['.js', '.json', '.jsx', 'css']
   },
+  
+  // プラグイン
   plugins: [
-    new webpack.EnvironmentPlugin({
-      NODE_ENV: env
-    }),
+    
     new webpack.ProvidePlugin({
       Promise: 'es6-promise-promise'
     }),
-    new webpack.ContextReplacementPlugin(
-      /moment[/\\]locale$/, /ja/
-    )
+    
+    // new webpack.ContextReplacementPlugin(
+    //   /moment[/\\]locale$/, /ja/
+    // ),
+    
   ],
-  devtool: 'source-map'
+  
+  // ソースマップを出力する
+  devtool: 'source-map',
+  
 };
-
-// Production ビルドの場合は圧縮する
-if (env === 'production') {
-  config.plugins.push(
-    new webpack.optimize.UglifyJsPlugin()
-  );
-}
-
-module.exports = config;
